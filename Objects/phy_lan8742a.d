85198aafceb6703a2dabd2060923c2cb1a8cc1d5
JorgeRos96 <86102864+JorgeRos96@users.noreply.github.com>
2021-07-01 18:44:02 +0200

    Add files via upload

---
+++ Objects/phy_lan8742a.d
@@ -0,0 +1,8 @@
+.\objects\phy_lan8742a.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS-Driver\2.6.1\ETH\PHY_LAN8742A.c
+.\objects\phy_lan8742a.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS-Driver\2.6.1\ETH\PHY_LAN8742A.h
+.\objects\phy_lan8742a.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Driver\Include\Driver_ETH_PHY.h
+.\objects\phy_lan8742a.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Driver\Include\Driver_ETH.h
+.\objects\phy_lan8742a.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Driver\Include\Driver_Common.h
+.\objects\phy_lan8742a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\phy_lan8742a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\phy_lan8742a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
---
+++ Objects/sntp.d
@@ -0,0 +1,11 @@
+.\objects\sntp.o: SNTP.c
+.\objects\sntp.o: SNTP.h
+.\objects\sntp.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\RTOS2\Include\cmsis_os2.h
+.\objects\sntp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\sntp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\sntp.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.12.0\Network\Include\rl_net.h
+.\objects\sntp.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.12.0\Network\Include\rl_net_ds.h
+.\objects\sntp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\sntp.o: .\RTE\_Target_1\RTE_Components.h
+.\objects\sntp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\time.h
+.\objects\sntp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
