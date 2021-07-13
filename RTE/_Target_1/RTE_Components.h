f3912253bc7a233450196fad26fd4dd93a5dd2f3
JorgeRos96 <86102864+JorgeRos96@users.noreply.github.com>
2021-07-13 18:47:20 +0200

    Add files via upload

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -19,20 +19,20 @@
 /*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.2 */
 #define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
         #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
-/*  Keil.MDK-Plus::Network:CORE:IPv4 Release:7.14.0 */
+/*  Keil.MDK-Pro::Network:CORE:IPv4 Release:7.14.0 */
 #define RTE_Network_Core                /* Network Core */
           #define RTE_Network_IPv4                /* Network IPv4 Stack */
           #define RTE_Network_Release             /* Network Release Version */
-/*  Keil.MDK-Plus::Network:Interface:ETH:7.14.0 */
+/*  Keil.MDK-Pro::Network:Interface:ETH:7.14.0 */
 #define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
 
-/*  Keil.MDK-Plus::Network:Service:SNTP Client:7.14.0 */
+/*  Keil.MDK-Pro::Network:Service:SNTP Client:7.14.0 */
 #define RTE_Network_SNTP_Client         /* Network SNTP Client */
-/*  Keil.MDK-Plus::Network:Service:Web Server Compact:HTTP:7.14.0 */
+/*  Keil.MDK-Pro::Network:Service:Web Server Compact:HTTP:7.14.0 */
 #define RTE_Network_Web_Server_RO       /* Network Web Server with Read-only Web Resources */
-/*  Keil.MDK-Plus::Network:Socket:TCP:7.14.0 */
+/*  Keil.MDK-Pro::Network:Socket:TCP:7.14.0 */
 #define RTE_Network_Socket_TCP          /* Network Socket TCP */
-/*  Keil.MDK-Plus::Network:Socket:UDP:7.14.0 */
+/*  Keil.MDK-Pro::Network:Socket:UDP:7.14.0 */
 #define RTE_Network_Socket_UDP          /* Network Socket UDP */
 /*  Keil::CMSIS Driver:Ethernet MAC:2.9 */
 #define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
